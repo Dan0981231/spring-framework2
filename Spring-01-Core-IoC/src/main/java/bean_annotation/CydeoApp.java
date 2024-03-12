dd372d5bdc56e3828c610c0f53078ba0180b7055
Dan0981231 <158116770+Dan0981231@users.noreply.github.com>
2024-03-12 15:45:18 -0400

    Changes

---
+++ Spring-01-Core-IoC/src/main/java/bean_annotation/CydeoApp.java
@@ -0,0 +1,24 @@
+package bean_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class, ConfigAny.class);
+
+        FullTimeMentor ft = container.getBean(FullTimeMentor.class);
+        ft.createAccount();
+
+        PartTimeMentor pt = container.getBean(PartTimeMentor.class);
+        pt.createAccount();
+
+        String str = container.getBean(String.class);
+        System.out.println(str);
+
+        Integer in = container.getBean(Integer.class);
+        System.out.println("in = " + in);
+
+    }
+}
