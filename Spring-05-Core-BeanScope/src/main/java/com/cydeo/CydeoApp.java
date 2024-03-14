6b2bd9c7e290d3d7bb48b01b3ac4ac5a1e677dde
Dan0981231 <158116770+Dan0981231@users.noreply.github.com>
2024-03-13 22:09:36 -0400

    5

---
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/CydeoApp.java
@@ -0,0 +1,31 @@
+package com.cydeo;
+
+import com.cydeo.config.ProjectConfig;
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+
+    public static void main(String[] args) {
+
+
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+//       CommentService cS1 = context.getBean(CommentService.class);
+//       CommentService cS2 = context.getBean(CommentService.class);
+//
+//        System.out.println(cS1);
+//        System.out.println(cS2);
+//
+//        System.out.println(cS1==cS2);
+
+
+
+    }
+}
