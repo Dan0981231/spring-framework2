12e8b5f91008595552d7b6f0e692d957679a507a
Dan0981231 <158116770+Dan0981231@users.noreply.github.com>
2024-04-28 15:06:01 -0400

    new

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,14 +3,18 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")
+//    @RequestMapping(value = "/register", method = RequestMethod.GET)
+
+    @GetMapping("register")
     public String register(Model model){
 
         model.addAttribute("students", DataGenerator.createStudent());
@@ -20,7 +24,8 @@ public class StudentController {
     return "student/register";
 }
 
-@RequestMapping("/welcome")
+//@RequestMapping("/welcome")
+    @GetMapping("welcome")
 public String info(){
         return "student/welcome";
 }
