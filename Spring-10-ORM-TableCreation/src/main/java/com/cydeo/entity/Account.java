fef917003b8273c48a542edb92797493243850f3
Dan0981231 <158116770+Dan0981231@users.noreply.github.com>
2024-05-06 22:24:08 -0400

    account

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Account.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Account.java
@@ -1,22 +1,19 @@
 package com.cydeo.entity;
-import javax.persistence.Entity;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.math.BigDecimal;
 
-@Entity
-public class Employee {  //employee
+@MappedSuperclass
+public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-
-    private String name;
-
-
-
-
-
+    private String owner;
+    private BigDecimal balance;
+    private BigDecimal interest;
 
 }
--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,15 +1,45 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.cydeo.enums.Gender;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
+@Table(name="students")
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "studentFirstName", length = 100)
     private String firstName;  // first_name
+    @Column(name = "studentLastName")
     private String lastName;
     private String email;
+
+    @Transient // removes field from the table
+    private String city;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthDate;
+    @Column(columnDefinition = "TIME")
+    private LocalTime birthTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDate birthDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+
+
+
+
+
+
+
+
+
 }
