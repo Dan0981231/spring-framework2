599853dd15e419393f246526133ed662af312d28
Dan0981231 <158116770+Dan0981231@users.noreply.github.com>
2024-05-07 21:24:37 -0400

    part 3

---
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.*;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+
+
+
+}
